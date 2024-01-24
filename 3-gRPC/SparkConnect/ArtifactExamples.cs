bc27657cc1705ac5af8fd6058b23e4b170ea7073
Ed Elliott <GoEddie@users.noreply.github.com>
2024-01-24 16:01:31 +0000

    Feature/spark connect 3 (#1)
    * explore gRPC

---
+++ 3-gRPC/SparkConnect/ArtifactExamples.cs
@@ -0,0 +1,71 @@
+using System.Text;
+using System.Text.Unicode;
+using Force.Crc32;
+using Google.Protobuf;
+using Grpc.Core;
+using Spark.Connect;
+
+namespace SparkConnect;
+public class ArtifactExamples
+{
+    public static async Task AddArtifact(SparkConnectService.SparkConnectServiceClient client, string sessionId)
+    {
+        var requestCall = client.AddArtifacts(new Metadata());
+        
+        var fileContent = Encoding.Unicode.GetBytes("print('hello, world!)'");
+
+        var request = new AddArtifactsRequest()
+        {
+            Batch = new AddArtifactsRequest.Types.Batch()
+            {
+                Artifacts =
+                {
+                    new AddArtifactsRequest.Types.SingleChunkArtifact()
+                    {
+                        Name = "cache/file.py",
+                        Data = new AddArtifactsRequest.Types.ArtifactChunk()
+                        {
+                            Data = ByteString.CopyFrom(fileContent), Crc = Crc32Algorithm.Compute(fileContent)
+                        }
+                    }
+                }
+            }, SessionId = sessionId
+        };
+
+        await requestCall.RequestStream.WriteAsync(request);
+
+        await requestCall.RequestStream.CompleteAsync();
+
+        await requestCall.ResponseAsync.WaitAsync(TimeSpan.FromSeconds(30));
+        
+        Console.WriteLine($"Was Crc Successful? {requestCall.ResponseAsync.Result.Artifacts[0].IsCrcSuccessful}");
+    }
+
+    public static void ArtifactStatus(SparkConnectService.SparkConnectServiceClient client, string sessionId)
+    {
+        var request = new ArtifactStatusesRequest()
+        {
+            Names = { "cache/file.py" }, SessionId = sessionId
+        };
+
+        var response = client.ArtifactStatus(request);
+        foreach (var status in response.Statuses)
+        {                                                      
+            Console.WriteLine($"Artifact: '{status.Key}' status: '{status.Value}' ");    
+        }
+    }
+    
+    public static void ArtifactNotUploadedStatus(SparkConnectService.SparkConnectServiceClient client, string sessionId)
+    {
+        var request = new ArtifactStatusesRequest()
+        {
+            Names = { "cache/AnotherFileNeverUploaded.jar" }, SessionId = sessionId
+        };
+
+        var response = client.ArtifactStatus(request);
+        foreach (var status in response.Statuses)
+        {         //The key exists and is correct but the status is missing in 3.5.0                                                  
+            Console.WriteLine($"Artifact: '{status.Key}' status: '{status.Value}' ");    
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,3 +12,6 @@ An example of how to include the Proto files in a .NET project and a .NET consol
 
 Post: https://the.agilesql.club/2024/01/using-spark-connect-from-csharp-on-databricks
 
+## Exploring the gRPC API - 3
+
+Post: https://the.agilesql.club/2024/01/exploring-the-spark-connect-grpc-api/
